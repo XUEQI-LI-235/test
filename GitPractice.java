8ac747717d93ac439df1bb167e735833d3fbb60e
XUEQI-LI-235 <li.xueqi.study@gmail.com>
2025-06-10 11:43:09 +0900

    create by xueqi

--- GitPractice.java
+++ GitPractice.java
@@ -2,6 +2,6 @@ public class GitPractice{
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("retest");
-
+        System.err.println("create by xueqili");
     }
 }
